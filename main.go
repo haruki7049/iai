fb414ac9e5134553eea557b09eec1812918c6e80
haruki7049 <tontonkirikiri@gmail.com>
2026-06-24 22:59:25 +0900

    Update

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	ebiten.SetWindowSize(game.WINDOW_WIDTH, game.WINDOW_HEIGHT)
 	ebiten.SetWindowTitle("Hello, World!")
-	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeDisabled)
 
 	if err := ebiten.RunGame(&game.Game{}); err != nil {
 		log.Fatal(err)
