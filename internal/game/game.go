3739b64d4ed10ff8173707f1859f745e14611e17
haruki7049 <tontonkirikiri@gmail.com>
2026-06-24 18:32:12 +0900

    Update

---
+++ internal/game/game.go
@@ -0,0 +1,28 @@
+package game
+
+import (
+	"github.com/hajimehoshi/bitmapfont/v4"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
+)
+
+var fontFace = text.NewGoXFace(bitmapfont.Face)
+
+const WINDOW_WIDTH = 640
+const WINDOW_HEIGHT = 480
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	textOp := &text.DrawOptions{}
+	textOp.LineSpacing = fontFace.Metrics().HLineGap + fontFace.Metrics().HAscent + fontFace.Metrics().HDescent
+	text.Draw(screen, "Hello!! It's \"iai\" game!!", fontFace, textOp)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth / 2, outsideHeight / 2
+}
--- main.go
+++ main.go
@@ -4,32 +4,15 @@ import (
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/haruki7049/iai/internal/game"
 )
 
-const WINDOW_WIDTH = 640
-const WINDOW_HEIGHT = 480
-
-type Game struct{}
-
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return outsideWidth / 2, outsideHeight / 2
-}
-
 func main() {
-	ebiten.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+	ebiten.SetWindowSize(game.WINDOW_WIDTH, game.WINDOW_HEIGHT)
 	ebiten.SetWindowTitle("Hello, World!")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
 
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(&game.Game{}); err != nil {
 		log.Fatal(err)
 	}
 }
